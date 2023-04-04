7d3eb18a7ce30d92db3a4302e983aed0fc4db1ec
Abir Dey <abir.dey2020@vitbhopal.ac.in>
2023-04-04 21:05:56 +0530

    first commit

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => (
+  <div className="flex justify-center items-center py-3">
+    <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-white-700" >
+    </div>
+
+  </div>
+);
+
+export default Loader;
